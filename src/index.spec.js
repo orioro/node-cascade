03863274c3899886bdb77d45af47c998d25bb741
Simon Fan <simon@orioro.design>
2021-01-22 16:00:24 -0300

    Adds typings

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,12 @@
-import { cascadeFind, test as testFn } from '../src'
+import {
+  cascadeFind,
+  cascadeExec,
+  test as testFn
+} from '../src'
 
-describe('cascadeFind(test, options, ...args)', () => {
+describe('cascadeFind(test, alternatives, ...args)', () => {
 	test('string', () => {
-    const options = [
+    const alternatives = [
       ['key1', 'value1'],
       ['key2', 'value2'],
       ['key3', 'value3'],
@@ -10,47 +14,100 @@ describe('cascadeFind(test, options, ...args)', () => {
       ['default value']
     ]
 
-    expect(cascadeFind(testFn, options, 'key3')).toEqual('value3')
-    expect(cascadeFind(testFn, options, 'key4')).toEqual('value4')
-    expect(cascadeFind(testFn, options, 'key9999')).toEqual('default value')
+    expect(cascadeFind(testFn, alternatives, 'key3')).toEqual('value3')
+    expect(cascadeFind(testFn, alternatives, 'key4')).toEqual('value4')
+    expect(cascadeFind(testFn, alternatives, 'key9999')).toEqual('default value')
 	})
 
   test('number', () => {
-    const options = [
+    const alternatives = [
       [1, 'value1'],
       [2, 'value2'],
       [4, 'value3'],
       [5, 'value4'],
       ['default value']
     ]
-    expect(cascadeFind(testFn, options, 1)).toEqual('value1')
-    expect(cascadeFind(testFn, options, 3)).toEqual('default value')
-    expect(cascadeFind(testFn, options, 1000)).toEqual('default value')
+    expect(cascadeFind(testFn, alternatives, 1)).toEqual('value1')
+    expect(cascadeFind(testFn, alternatives, 3)).toEqual('default value')
+    expect(cascadeFind(testFn, alternatives, 1000)).toEqual('default value')
   })
 
   test('regexp', () => {
-    const options = [
+    const alternatives = [
       [/^abcde$/, 'value1'],
       [/^abcde/, 'value2'],
       [/^abcd/, 'value3'],
       [/^abc/, 'value4'],
       ['default value']
     ]
-    expect(cascadeFind(testFn, options, 'abcde')).toEqual('value1')
-    expect(cascadeFind(testFn, options, 'abcdef')).toEqual('value2')
-    expect(cascadeFind(testFn, options, 'abcxyz')).toEqual('value4')
-    expect(cascadeFind(testFn, options, 'xyz')).toEqual('default value')
+    expect(cascadeFind(testFn, alternatives, 'abcde')).toEqual('value1')
+    expect(cascadeFind(testFn, alternatives, 'abcdef')).toEqual('value2')
+    expect(cascadeFind(testFn, alternatives, 'abcxyz')).toEqual('value4')
+    expect(cascadeFind(testFn, alternatives, 'xyz')).toEqual('default value')
   })
 
   test('function', () => {
-    const options = [
+    const alternatives = [
       [value => value < 20, '<20'],
       [value => value >= 20 && value < 50, '>=20 && <50'],
       [value => value > 50, '>50']
     ]
 
-    expect(cascadeFind(testFn, options, 19)).toEqual('<20')
-    expect(cascadeFind(testFn, options, 20)).toEqual('>=20 && <50')
-    expect(cascadeFind(testFn, options, 51)).toEqual('>50')
+    expect(cascadeFind(testFn, alternatives, 19)).toEqual('<20')
+    expect(cascadeFind(testFn, alternatives, 20)).toEqual('>=20 && <50')
+    expect(cascadeFind(testFn, alternatives, 51)).toEqual('>50')
+  })
+})
+
+describe('cascadeExec(test, alternatives, ...args)', () => {
+  test('', () => {
+    const alternatives = [
+      [
+        item => item.type === 'book',
+        item => `${item.title} - por ${item.authors.join(' e ')}`
+      ],
+      [
+        item => item.type === 'plant',
+        item => `${item.name} (${item.scientificName})`
+      ],
+      [
+        item => item.type === 'person',
+        item => `${item.firstName} ${item.middleName} ${item.lastName}, ${item.profession}`
+      ],
+      [
+        item => `Unknown: ${(item.name || item.title)}`
+      ]
+    ]
+
+    const summarize = item => cascadeExec(testFn, alternatives, item)
+
+    expect(summarize({
+      type: 'book',
+      title: 'Psicogênese da Língua Escrita',
+      authors: ['Emília Ferreiro', 'Ana Teberosky']
+    }))
+    .toEqual('Psicogênese da Língua Escrita - por Emília Ferreiro e Ana Teberosky')
+
+    expect(summarize({
+      type: 'plant',
+      name: 'Samambaiaçu',
+      scientificName: 'Dicksonia sellowiana'
+    }))
+    .toEqual('Samambaiaçu (Dicksonia sellowiana)')
+
+    expect(summarize({
+      type: 'person',
+      firstName: 'João',
+      lastName: 'Nunes',
+      middleName: 'Silveira',
+      profession: 'Desenvolvedor de software'
+    }))
+    .toEqual('João Silveira Nunes, Desenvolvedor de software')
+
+    expect(summarize({
+      type: 'something_else',
+      title: 'Something else'
+    }))
+    .toEqual('Unknown: Something else')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-export type Criteria = (...args:any[]) => boolean | any
-export type Option = [Criteria, any] | [any]
+export type Criteria = (
+  (...args:any[]) => boolean |
+  any
+)
+export type Alternative = [Criteria, any] | [any]
+export type ExecutableAlternative = (
+  [Criteria, (...args:any[]) => any] |
+  [(...args:any[]) => any]
+)
 
-export const test = (criteria, ...args) => {
+export type TestFunction = (
+  criteria:Criteria,
+  ...args:any
+) => boolean
+
+export const test:TestFunction = (
+  criteria,
+  ...args
+) => {
   return (criteria instanceof RegExp)
     ? typeof args[0] === 'string' && criteria.test(args[0])
     : typeof criteria === 'function'
@@ -9,30 +24,49 @@ export const test = (criteria, ...args) => {
       : criteria === args[0]
 }
 
-const _optionTest = (test, option, args) => (
-  option.length === 1 ||
+const _alternativeTest = (
+  test:TestFunction,
+  alternative:Alternative,
+  args:any[]
+) => (
+  alternative.length === 1 ||
   (
-    option.length === 2 &&
-    test(option[0], ...args)
+    alternative.length === 2 &&
+    test(alternative[0], ...args)
   )
 )
 
-const _optionValue = (option) => option.length === 1 ? option[0] : option[1]
+const _alternativeValue = (
+  alternative:Alternative
+) => (
+  alternative.length === 1
+    ? alternative[0]
+    : alternative[1]
+)
 
-export const cascadeFind = (test, options, ...args) => {
-  const option = options.find(option => _optionTest(test, option, args))
+export const cascadeFind = (
+  test:TestFunction,
+  alternatives:Alternative[],
+  ...args:any[]
+):any => {
+  const alternative = alternatives
+    .find(alternative => _alternativeTest(test, alternative, args))
 
-  return option ? _optionValue(option) : undefined
+  return alternative ? _alternativeValue(alternative) : undefined
 }
 
-export const cascadeExec = (test, options, ...args) => (
-  cascadeFind(test, options, ...args)(...args)
-)
+export const cascadeExec = (
+  test:TestFunction,
+  alternatives:ExecutableAlternative[],
+  ...args:any[]
+):any => cascadeFind(test, alternatives, ...args)(...args)
 
-export const cascadeFilter = (test, options, ...args) => (
-  options.reduce((acc, option) => (
-    _optionTest(test, option, args)
-      ? [...acc, _optionValue(option)]
-      : acc
-  ), [])
-)
+export const cascadeFilter = (
+  test:TestFunction,
+  alternatives:Alternative[],
+  ...args:any[]
+):any => alternatives.reduce((acc, alternative) => (
+  _alternativeTest(test, alternative, args)
+    ? [...acc, _alternativeValue(alternative)]
+    : acc
+), [])
