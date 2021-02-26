5e862cdfd0a111dd41e38ac3407f63d257739207
Simon Fan <simon@orioro.design>
2021-02-26 15:06:02 -0300

    chore: add code linting workflow

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+// https://github.com/jest-community/eslint-plugin-jest/issues/555
+/* eslint-disable jest/no-export, jest/expect-expect, jest/valid-title */
+
 /**
  * Function that returns boolean
- * 
+ *
  * @typedef Criteria
  */
-export type Criteria = (
-  (...args:any[]) => boolean |
-  any
-)
+export type Criteria = (...args: any[]) => boolean | any
 
 /**
  * Tuple consisting of either:
@@ -22,59 +22,43 @@ export type Alternative = [Criteria, any] | [any]
  * - `[Function]`
  * @typedef {[Criteria, any] | [any]} Alternative
  */
-export type ExecutableAlternative = (
-  [Criteria, (...args:any[]) => any] |
-  [(...args:any[]) => any]
-)
+export type ExecutableAlternative =
+  | [Criteria, (...args: any[]) => any]
+  | [(...args: any[]) => any]
 
 /**
  * Function that interprets a given criteria.
  * Takes as arguments:
  * - {Criteria} criteria
  * - ...args Remaining arguments
- * 
+ *
  * @typedef TestFunction
  */
-export type TestFunction = (
-  criteria:Criteria,
-  ...args:any
-) => boolean
+export type TestFunction = (criteria: Criteria, ...args: any) => boolean
 
 /**
  * @function test
  * @param {Criteria} criteria
  * @param {...args[]} args
  */
-export const test:TestFunction = (
-  criteria,
-  ...args
-) => {
-  return (criteria instanceof RegExp)
+export const test: TestFunction = (criteria, ...args) => {
+  return criteria instanceof RegExp
     ? typeof args[0] === 'string' && criteria.test(args[0])
     : typeof criteria === 'function'
-      ? criteria(...args)
-      : criteria === args[0]
+    ? criteria(...args)
+    : criteria === args[0]
 }
 
 const _alternativeTest = (
-  test:TestFunction,
-  alternative:Alternative,
-  args:any[]
-) => (
+  test: TestFunction,
+  alternative: Alternative,
+  args: any[]
+) =>
   alternative.length === 1 ||
-  (
-    alternative.length === 2 &&
-    test(alternative[0], ...args)
-  )
-)
+  (alternative.length === 2 && test(alternative[0], ...args))
 
-const _alternativeValue = (
-  alternative:Alternative
-) => (
-  alternative.length === 1
-    ? alternative[0]
-    : alternative[1]
-)
+const _alternativeValue = (alternative: Alternative) =>
+  alternative.length === 1 ? alternative[0] : alternative[1]
 
 /**
  * @function cascadeFind
@@ -84,12 +68,13 @@ const _alternativeValue = (
  * @returns {*}
  */
 export const cascadeFind = (
-  test:TestFunction,
-  alternatives:Alternative[],
-  ...args:any[]
-):any => {
-  const alternative = alternatives
-    .find(alternative => _alternativeTest(test, alternative, args))
+  test: TestFunction,
+  alternatives: Alternative[],
+  ...args: any[]
+): any => {
+  const alternative = alternatives.find((alternative) =>
+    _alternativeTest(test, alternative, args)
+  )
 
   return alternative ? _alternativeValue(alternative) : undefined
 }
@@ -102,10 +87,10 @@ export const cascadeFind = (
  * @returns {*}
  */
 export const cascadeExec = (
-  test:TestFunction,
-  alternatives:ExecutableAlternative[],
-  ...args:any[]
-):any => cascadeFind(test, alternatives, ...args)(...args)
+  test: TestFunction,
+  alternatives: ExecutableAlternative[],
+  ...args: any[]
+): any => cascadeFind(test, alternatives, ...args)(...args)
 
 /**
  * @function cascadeFind
@@ -115,11 +100,14 @@ export const cascadeExec = (
  * @returns {*[]}
  */
 export const cascadeFilter = (
-  test:TestFunction,
-  alternatives:Alternative[],
-  ...args:any[]
-):any => alternatives.reduce((acc, alternative) => (
-  _alternativeTest(test, alternative, args)
-    ? [...acc, _alternativeValue(alternative)]
-    : acc
-), [])
+  test: TestFunction,
+  alternatives: Alternative[],
+  ...args: any[]
+): any =>
+  alternatives.reduce(
+    (acc, alternative) =>
+      _alternativeTest(test, alternative, args)
+        ? [...acc, _alternativeValue(alternative)]
+        : acc,
+    []
+  )
